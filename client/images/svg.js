f6e4dd2df265f6ef0c3b84bab154a5d4829614f0
Dimas Elang <dimaselang033@gmail.com>
2022-07-12 14:38:29 +0700

    asset

---
+++ client/images/svg.js
@@ -0,0 +1,33 @@
+let svg = {
+    'connect':`<svg class="m-auto" version=" 1.0" xmlns="http://www.w3.org/2000/svg" width="80%" height="80%"
+                viewBox="0 0 100.000000 100.000000" preserveAspectRatio="xMidYMid meet">
+                <metadata>
+                    Created by potrace 1.10, written by Peter Selinger 2001-2011
+                </metadata>
+                <g transform="translate(0.000000,100.000000) scale(0.050000,-0.050000)">
+                    <path fill="#02ad77"
+                        d="M902 1172 c-173 -96 -101 -372 98 -372 101 0 200 98 200 198 0 150 -166 247 -298 174z"
+                        class="svg-elem-1" />
+                    <path fill="#02ad77"
+                        d="M510 1343 c-141 -213 -141 -473 -1 -685 118 -178 221 -76 119 117 -90 169 -91 277 -7 437 103 196 7 309 -111 131z"
+                        class="svg-elem-3" />
+                    <path fill="#02ad77"
+                        d="M1324 1387 c-15 -26 -3 -66 50 -166 91 -172 88 -303 -12 -464 -74 -120 -66 -167 29 -167 125 0 239 308 193 524 -46 222 -199 382 -260 273z"
+                        class="svg-elem-3" />
+                    <path fill="#02ad77"
+                        d="M218 1635 c-289 -396 -288 -874 1 -1271 51 -70 73 -85 117 -80 74 8 82 79 18 162 -249 328 -250 804 -1 1101 52 62 60 112 23 149 -44 44 -95 25 -158 -61z"
+                        class="svg-elem-5" />
+                    <path fill="#02ad77"
+                        d="M1624 1696 c-37 -37 -29 -82 28 -157 246 -330 244 -764 -6 -1093 -64 -83 -56 -154 18 -162 133 -16 335 417 335 716 0 348 -253 818 -375 696z"
+                        class="svg-elem-5" />
+                </g>
+            </svg>`,
+    'disconnect':`<svg version="1.0" class="m-auto" xmlns="http://www.w3.org/2000/svg" width="80%" height="80%" viewBox="0 0 100.000000 100.000000" preserveAspectRatio="xMidYMid meet">  
+                        <g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none"> 
+                            <path fill="red" d="M496 814 c-34 -11 -65 -31 -103 -70 -52 -52 -56 -54 -91 -46 -77 17 -154 -28 -168 -98 -4 -19 -16 -32 -36 -41 -41 -17 -86 -81 -94 -136 -13 -86 32 -169 111 -204 36 -17 72 -19 388 -19 291 0 354 3 384 15 50 21 72 42 94 90 45 98 -1 203 -106 241 -26 9 -31 17 -36 56 -15 99 -88 185 -180 212 -64 19 -99 19 -163 0z m-24 -301 l48 -47 50 49 c39 38 52 47 62 37 10 -10 1 -23 -37 -62 l-49 -50 49 -50 c38 -39 47 -52 37 -62 -10 -10 -23 -1 -62 37 l-50 49 -50 -49 c-39 -38 -52 -47 -62 -37 -10 10 -1 23 37 62 l49 50 -48 49 c-27 27 -45 54 -42 60 11 17 17 14 68 -36z"></path> 
+                        </g> 
+                    </svg>`,
+    'delete': `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+  </svg>`
+}
